e9137ee4d3d55d865e2b91017261c86ccd788c2f
Steve Calvert <scalvert@mobify.com>
2015-04-13 10:24:16 -0700

    Adding split test

--- template-split/build/buildConfig.js
+++ template-split/build/buildConfig.js
@@ -1,6 +1,6 @@
 define([], function() {
     return {
-        buildDate: 1428602192541,
-        cacheHashManifest: {"css/stylesheet.css":"dc254043","css/stylesheet.css.map":"912b61a3","js/ui.js":"cc625f4e","js/ui.js.map":"85ea2e1a","bower_components/deckard/dist/deckard.min.js":"8b1854e7"}
+        buildDate: 1428945482623,
+        cacheHashManifest: {"css/stylesheet.css":"dc254043","css/stylesheet.css.map":"912b61a3","js/ui.js":"cc625f4e","js/ui.js.map":"fdd384c0","bower_components/deckard/dist/deckard.min.js":"8b1854e7"}
     }
 });
